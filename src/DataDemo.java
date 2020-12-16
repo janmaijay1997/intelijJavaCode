4a5c43fec947134fdbe62305e467305cd419ac66
janmaijay1997 <30903817+janmaijay1997@users.noreply.github.com>
2020-12-16 22:07:23 +0530

    first commit

--- src/DataDemo.java
+++ src/DataDemo.java
@@ -1,6 +1,9 @@
+import java.util.Date;
+
 public class DataDemo {
 
     public static void main (String args[]){
         System.out.println("testing ");
+        System.out.println(new Date(System.currentTimeMillis()).);
     }
 }
